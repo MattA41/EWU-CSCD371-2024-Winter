a1e8471f17bc8c9564d621722a29adebfeb5dfc9
madams41 <madams41@ewu.edu>
2024-01-16 15:03:45 -0800

    added notes to code

--- ClassDemo/ClassDemo.Tests/ProgamTests.cs
+++ ClassDemo/ClassDemo.Tests/ProgamTests.cs
@@ -2,8 +2,9 @@ namespace ClassDemo.Tests;
 
 public class ProgramTests
 {
+    // If wanted to do cleanup run it before initalizing
     Program _Program = new Program();
-
+    // we dont need the instance count method just used in class to show it re-intitalize every time
     int _InstanceCount = 0;
     [Fact]
     public void Login_inigoMontoyaWithGoodPassword_SuccsessfulLogin()
@@ -11,7 +12,7 @@ public class ProgramTests
         Assert.Equal(0, _InstanceCount);
         string username = "Inigo.Montoya";
         string password =  "goodpassword";
-        
+
         _InstanceCount++;
 
         Assert.True(_Program.Login(username, password));
