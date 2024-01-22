6cd285c8fa9a268302970e532b8d37ae4daeccbe
madams41 <madams41@ewu.edu>
2024-01-22 10:42:37 -0800

    more changes to class files

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,10 +1,16 @@
 ﻿namespace Logger;
 
-public class LogFactory
+public class LogFactory :BaseLogger
 {
     public BaseLogger CreateLogger(string className)
     {
-
+        this.className = className;
         return null;
     }
+
+    public override void Log(LogLevel logLevel, string message)
+    {
+        throw new System.NotImplementedException();
+    }
+
 }
