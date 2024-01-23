1bb8f7d1b2800524499fe2b32a063b8dac8d5503
madams41 <madams41@ewu.edu>
2024-01-23 14:59:52 -0800

    fixed spacing in base logger mixins

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -7,50 +7,22 @@ public static class BaseLoggerMixins
     public static void Error(this BaseLogger? logger, string message, params object[] arguments)
     {
         ArgumentNullException.ThrowIfNull(logger);
-        /*if (arguments == null)
-        {
-            throw new ArgumentNullException (nameof(arguments) + " arguments is null");
-        }
-        else
-        { */
-            logger.Log(LogLevel.Error, message + string.Join(" ", arguments));
-        //}
+        logger.Log(LogLevel.Error, message + string.Join(" ", arguments));
     }
     public static void Warning(this BaseLogger? logger, string message, params object[] arguments)
     {
         ArgumentNullException.ThrowIfNull(logger);
-        /*if (arguments == null)
-        {
-            throw new ArgumentNullException(nameof(arguments) + " arguments is null");
-        }
-        else
-        { */
-            logger.Log(LogLevel.Warning, message + string.Join(" ", arguments));
-        //}
+        logger.Log(LogLevel.Warning, message + string.Join(" ", arguments));
     }
     public static void Information(this BaseLogger? logger, string message, params object[] arguments)
     {
         ArgumentNullException.ThrowIfNull(logger);
-        /*if (arguments == null)
-        {
-            throw new ArgumentNullException(nameof(arguments) + " arguments is null");
-        }*/
-        //else
-        //{
-            logger.Log(LogLevel.Information, message + string.Join(" ", arguments));
-        //}
+        logger.Log(LogLevel.Information, message + string.Join(" ", arguments));
     }
     public static void Debug(this BaseLogger? logger, string message, params object[] arguments)
     {
         ArgumentNullException.ThrowIfNull(logger);
-        //if (arguments == null)
-        /*{
-            throw new ArgumentNullException(nameof(arguments) + " arguments is null");
-        }
-        else
-        { */
-            logger.Log(LogLevel.Debug, message + string.Join(" ", arguments));
-        //}
+        logger.Log(LogLevel.Debug, message + string.Join(" ", arguments));
     }
 
 }
