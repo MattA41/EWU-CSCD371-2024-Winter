9105d3372e8eeabcaef716869d18ca7627d61f64
madams41 <madams41@ewu.edu>
2024-01-26 12:26:06 -0800

    fixed main

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -10,5 +10,6 @@ class Program
         //Feel free to use your own setup here - this is just provided as an example
         //new Jester(new SomeReallyCoolOutputClass(), new SomeJokeServiceClass()).TellJoke();
         Jester.Print(Jester.TellJoke());
+        
     }
 }
