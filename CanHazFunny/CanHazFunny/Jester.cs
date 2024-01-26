ebe165722b700e3ff42542a7b32f20636b92b832
rkdeboer <145617897+rkdeboer@users.noreply.github.com>
2024-01-26 12:17:49 -0800

    Auto stash before merge of "Assignment3" and "origin/Assignment3"

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +10,22 @@ namespace CanHazFunny
     {
         public void Print(String Joke);
     }
-    internal class Jester
+    public class Jester
     {
         public static void Print(String Joke) 
         {
             ArgumentNullException.ThrowIfNull(Joke);
             Console.WriteLine(Joke);
+        }
+        public string TellJoke()
+        {
+            JokeService jokeGenerator = new JokeService();
+            string joke = jokeGenerator.GetJoke();
+            while(joke.ToLower().Equals("Chuck Norris".ToLower()))
+            {
+                joke = jokeGenerator.GetJoke();
+            }
+            return joke;
         }
     }
 }
