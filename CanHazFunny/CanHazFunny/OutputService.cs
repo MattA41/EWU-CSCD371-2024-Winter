4c9c78e7d3110f5eccc1ecbd0899d1ce00ec50a6
rkdeboer <145617897+rkdeboer@users.noreply.github.com>
2024-01-30 11:35:40 -0800

    Added output service

---
+++ CanHazFunny/CanHazFunny/OutputService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanHazFunny
+{
+    internal class OutputService : IOutPutJoke
+    {
+        public void Print(string Joke)
+        {
+            Console.WriteLine(Joke);
+
+        }
+    }
+}
--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -9,7 +9,10 @@ public class Program
         //ArgumentNullException.ThrowIfNull(args);
         //Feel free to use your own setup here - this is just provided as an example
         //new Jester(new SomeReallyCoolOutputClass(), new SomeJokeServiceClass()).TellJoke();
-       // Jester.Print(Jester.TellJoke());
+       OutputService outputService = new OutputService();
+        JokeService jokeService = new JokeService();
+        Jester jest = new Jester(jokeService, outputService);
+        jest.TellJoke();
         
     }
 }
