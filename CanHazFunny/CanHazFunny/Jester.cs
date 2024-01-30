93a05430276e29498ba8d6128e9788f78ddee48f
rkdeboer <145617897+rkdeboer@users.noreply.github.com>
2024-01-29 16:59:06 -0800

    Changed to file-scoped namespace

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -5,8 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace CanHazFunny
-{   
+namespace CanHazFunny; 
     interface IOutPutJoke
     {
         public void Print(String Joke);
@@ -33,4 +32,3 @@ namespace CanHazFunny
             return joke;
         }
     }
-}
--- CanHazFunny/CanHazFunny/JokeServiceInterface.cs
+++ CanHazFunny/CanHazFunny/JokeServiceInterface.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CanHazFunny
-{
+namespace CanHazFunny;
     public interface IJokeServiceInterface
     {
         public string GetJoke();
     }
-}
+
