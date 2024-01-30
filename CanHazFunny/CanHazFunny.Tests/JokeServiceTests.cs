ce9d263e3661d86e9556ee0cc71192d867a03d67
madams41 <madams41@ewu.edu>
2024-01-30 11:26:28 -0800

    Added tests for joke service

---
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CanHazFunny.Tests
+{
+    public  class JokeServiceTests
+    {
+        [Fact]
+        public void TestJokeService()
+        {
+            JokeService jokeService = new JokeService();
+            string joke = jokeService.GetJoke();
+            Assert.NotNull(joke);
+        }
+    }
+}
