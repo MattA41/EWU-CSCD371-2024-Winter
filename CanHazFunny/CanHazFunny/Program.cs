e63463106ebe0ba714c56623882b9580566cbf57
madams41 <madams41@ewu.edu>
2024-01-30 12:31:47 -0800

    removed unneeded comments

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -6,9 +6,6 @@ public class Program
 {
     public static void Main()
     {
-        //ArgumentNullException.ThrowIfNull(args);
-        //Feel free to use your own setup here - this is just provided as an example
-        //new Jester(new SomeReallyCoolOutputClass(), new SomeJokeServiceClass()).TellJoke();
        OutputService outputService = new();
         JokeService jokeService = new();
         Jester jest = new(jokeService, outputService);
