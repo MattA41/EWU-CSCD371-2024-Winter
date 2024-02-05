861f9f4d9188b27f9407853497da69475ddbd72d
rkdeboer <145617897+rkdeboer@users.noreply.github.com>
2024-02-05 10:42:14 -0800

    Added Records
    Book, Employee, Student

---
+++ Logger/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger;
+public record Employee(Guid Id, string FirstName, string LastName, string Department) : IEntity
+{
+
+    public string Name => $"{FirstName} {LastName} ({Department})";
+}
