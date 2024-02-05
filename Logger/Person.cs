9289c060948e617f2d7c3d134f62f5bc3623c17d
rkdeboer <145617897+rkdeboer@users.noreply.github.com>
2024-02-05 14:07:59 -0800

    Person changes

--- Logger/Person.cs
+++ Logger/Person.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,7 @@ namespace Logger;
     public record class Person(FullName FullName) : BaseEntity
     {
         
-        public override string Name { get => fullName.Name; }
-        public override Guid Id { get => throw new NotImplementedException(); init => throw new NotImplementedException(); }
+        public override string Name { get => FullName.Name; }
+
     }
 
