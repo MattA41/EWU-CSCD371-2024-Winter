080d11a61a15ae518e21f313f1c7fa106c9e868a
rkdeboer <145617897+rkdeboer@users.noreply.github.com>
2024-02-09 13:39:39 -0800

    Node Class base

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace GenericsHomework
 {
-    public class Class1
+    public class Node<T>
     {
+        public T Value { get; }
+        public Node<T> Next { get; private set; }
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = this; // Points back to itself initially
+        }
+
+        public void SetNext(Node<T> next)
+        {
+            Next = next;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
     }
+
 }
