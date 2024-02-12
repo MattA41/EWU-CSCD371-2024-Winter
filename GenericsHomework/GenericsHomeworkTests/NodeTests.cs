82d17c29e1dada94fec85b47ab61308553bb9fbe
madams41 <madams41@ewu.edu>
2024-02-12 11:52:35 -0800

    fixed more issues

--- GenericsHomework/GenericsHomeworkTests/NodeTests.cs
+++ GenericsHomework/GenericsHomeworkTests/NodeTests.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using Xunit;
 
 namespace GenericsHomeworkTests
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        [Fact]
         public void TestMethod1()
         {
         }
