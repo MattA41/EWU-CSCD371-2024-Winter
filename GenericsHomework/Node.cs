8a3d684ff87873d9fcd8e7c808b82237d03e0f78
rkdeboer <145617897+rkdeboer@users.noreply.github.com>
2024-02-13 15:03:09 -0800

    FileScoped namespace

--- GenericsHomework/Node.cs
+++ GenericsHomework/Node.cs
@@ -4,89 +4,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GenericsHomework
+namespace GenericsHomework;
+public class Node<T>
 {
-    public class Node<T>
+    public T Value { get; }
+    public Node<T> Next { get; private set; }
+
+    public Node(T value)
     {
-        public T Value { get; }
-        public Node<T> Next { get; private set; }
+        Value = value;
+        Next = this;
+    }
 
-        public Node(T value)
-        {
-            Value = value;
-            Next = this;
-        }
+    public void SetNext(Node<T> next)
+    {
+        // I think having the check has made it so that next cannot 
+        ArgumentNullException.ThrowIfNull(next);
+        Next = next;
+    }
 
-        public void SetNext(Node<T> next)
+    public override string ToString()
+    {
+        Node<T> dummy = new(this.Value)
         {
-            // I think having the check has made it so that next cannot 
-            ArgumentNullException.ThrowIfNull(next);
-            Next = next;
-        }
-
-        public override string ToString()
+            Next = this
+        };
+        string ret = "";
+        // goes through linked list starting at node called
+        while (dummy.Next != (dummy.Next).Next)
         {
-            Node<T> dummy = new(this.Value)
+            if (dummy.Next.Value != null)
             {
-                Next = this
-            };
-            string ret = "";
-            // goes through linked list starting at node called
-            while (dummy.Next != (dummy.Next).Next)
-            {
-                if (dummy.Next.Value != null)
-                {
 
-                    ret += dummy.Next.Value.ToString();
-                }
-                dummy.Next = dummy.Next.Next;
-
+                ret += dummy.Next.Value.ToString();
             }
-            
-            return ret;
+            dummy.Next = dummy.Next.Next;
+
         }
-        public void Append(T value)
+
+        return ret;
+    }
+    public void Append(T value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (Exists(value))
         {
-            ArgumentNullException.ThrowIfNull(value);
-            if (Exists(value))
-            {
-                return;
-            }
-            Node<T> dummy = new(value)
-            {
-                Next = this
-            };
-            while (dummy.Next != (dummy.Next).Next)
-            {
-                dummy.Next = dummy.Next.Next;
-            }
-            dummy.Next.Next = dummy;
+            return;
         }
-        public void Clear()
+        Node<T> dummy = new(value)
         {
-            Next = this;
-            //All we need to do to clear the list is reconnect the next to the node itself. 
-            //Since the other nodes are disconnected from the head node after this method executes, the remaining nodes are eligible for garbage collection
-            //Since there is nothing to reference them, we do not need to worry about what happens to them. They are taken care of in garbage collection
+            Next = this
+        };
+        while (dummy.Next != (dummy.Next).Next)
+        {
+            dummy.Next = dummy.Next.Next;
         }
-        public bool Exists(T value)
+        dummy.Next.Next = dummy;
+    }
+    public void Clear()
+    {
+        Next = this;
+        //All we need to do to clear the list is reconnect the next to the node itself. 
+        //Since the other nodes are disconnected from the head node after this method executes, the remaining nodes are eligible for garbage collection
+        //Since there is nothing to reference them, we do not need to worry about what happens to them. They are taken care of in garbage collection
+    }
+    public bool Exists(T value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        Node<T> dummy = new(value)
         {
-            ArgumentNullException.ThrowIfNull(value);
-            Node<T> dummy = new(value)
-            {
-                Next = this
-            };
-            while (dummy.Next != (dummy.Next).Next)
-            {   if (dummy.Next.Value != null)
-                {
-                    if (dummy.Next.Value.Equals(value))
-                    {
-                        return true;
-                    }
-                }
-                dummy.Next = dummy.Next.Next;
-            
-            }
+            Next = this
+        };
+        while (dummy.Next != (dummy.Next).Next)
+        {
             if (dummy.Next.Value != null)
             {
                 if (dummy.Next.Value.Equals(value))
@@ -94,8 +84,18 @@ namespace GenericsHomework
                     return true;
                 }
             }
-            return false;
-        }
-    }
+            dummy.Next = dummy.Next.Next;
 
+        }
+        if (dummy.Next.Value != null)
+        {
+            if (dummy.Next.Value.Equals(value))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+
--- GenericsHomeworkTests/NodeTests.cs
+++ GenericsHomeworkTests/NodeTests.cs
@@ -1,102 +1,101 @@
 using GenericsHomework;
 using Xunit;
 
-namespace GenericsHomeworkTests
+namespace GenericsHomeworkTests;
+public class NodeTests
 {
-    public class NodeTests
+    [Fact]
+    public void SetNext_Null()
     {
-        [Fact]
-        public void SetNext_Null()
-        {
-            Node<String> node1 = new("string");
-           
-            Assert.Throws<ArgumentNullException>(() => node1.SetNext(null!));
-        }
+        Node<String> node1 = new("string");
 
-        [Fact]
-        public void SetNext_GoodVal()
-        {
-            Node<String> node1 = new("String1");
-            Node<String> node2 = new("String2");
-            node1.SetNext(node2);
-            Assert.Equal(node1.Next, node2);
-        }
-        [Fact]
-        public void Clear_DeletesList()
-        {
-            Node<String> node1 = new("String");
-            Node<String> node2 = new("String 2");
-            Node<String> node3 = new("String 3");
-            node1.SetNext(node2);
-            node2.SetNext(node3);
-            node1.Clear();
-            Assert.Equal(node1, node1.Next);
-        }
+        Assert.Throws<ArgumentNullException>(() => node1.SetNext(null!));
+    }
 
-        [Fact]
-        public void Exists_TellsIfInList_Middle()
-        {
-            Node<String> node1 = new("String");
-            Node<String> node2 = new("String 2");
-            Node<String> node3 = new("String 3");
-            Node<String> node4 = new("String 4");
-            node1.SetNext(node2);
-            node2.SetNext(node3);
-            node3.SetNext(node4);
-            
-            //used Assert.True because exists returns true or false if the item exits
-            Assert.True(node1.Exists("String 3"));
-        }
+    [Fact]
+    public void SetNext_GoodVal()
+    {
+        Node<String> node1 = new("String1");
+        Node<String> node2 = new("String2");
+        node1.SetNext(node2);
+        Assert.Equal(node1.Next, node2);
+    }
+    [Fact]
+    public void Clear_DeletesList()
+    {
+        Node<String> node1 = new("String");
+        Node<String> node2 = new("String 2");
+        Node<String> node3 = new("String 3");
+        node1.SetNext(node2);
+        node2.SetNext(node3);
+        node1.Clear();
+        Assert.Equal(node1, node1.Next);
+    }
 
-        [Fact]
-        public void Exists_TellsIfInList_End()
-        {
-            Node<String> node1 = new("String");
-            Node<String> node2 = new("String 2");
-            Node<String> node3 = new("String 3");
-            Node<String> node4 = new("String 4");
-            node1.SetNext(node2);
-            node2.SetNext(node3);
-            node3.SetNext(node4);
+    [Fact]
+    public void Exists_TellsIfInList_Middle()
+    {
+        Node<String> node1 = new("String");
+        Node<String> node2 = new("String 2");
+        Node<String> node3 = new("String 3");
+        Node<String> node4 = new("String 4");
+        node1.SetNext(node2);
+        node2.SetNext(node3);
+        node3.SetNext(node4);
 
-            //used Assert.True because exists returns true or false if the item exits
-            Assert.True(node1.Exists("String 4"));
-        }
+        //used Assert.True because exists returns true or false if the item exits
+        Assert.True(node1.Exists("String 3"));
+    }
 
-        [Fact]
-        public void Exists_NullVal()
-        {
-            Node<String> node1 = new("String");
-            Node<String> node2 = new("String 2");
-            Node<String> node3 = new("String 3");
-            node1.SetNext(node2);
-            node2.SetNext(node3);
-            Assert.Throws<ArgumentNullException>(() => node1.Exists(null!));
-        }
+    [Fact]
+    public void Exists_TellsIfInList_End()
+    {
+        Node<String> node1 = new("String");
+        Node<String> node2 = new("String 2");
+        Node<String> node3 = new("String 3");
+        Node<String> node4 = new("String 4");
+        node1.SetNext(node2);
+        node2.SetNext(node3);
+        node3.SetNext(node4);
 
-        [Fact]
-        public void Append_TestIfAddsLast()
-        {
-            Node<String> node1 = new("String 1");
-            Node<String> node2 = new("String 2");
-            Node<String> node3 = new("String 3");
-            node1.SetNext(node2);
-            node2.SetNext(node3);
-            string data = "String4";
-            node1.Append(data);
+        //used Assert.True because exists returns true or false if the item exits
+        Assert.True(node1.Exists("String 4"));
+    }
 
-            Assert.Equal(node3.Next.Value, data);
-        }
-        [Fact]
-        public void Append_TestNullValue()
-        {
-            Node<String> node1 = new("String");
-            Node<String> node2 = new("String 2");
-            Node<String> node3 = new("String 3");
-            node1.SetNext(node2);
-            node2.SetNext(node3);
-            Assert.Throws<ArgumentNullException>(() => node1.Append(null!));
+    [Fact]
+    public void Exists_NullVal()
+    {
+        Node<String> node1 = new("String");
+        Node<String> node2 = new("String 2");
+        Node<String> node3 = new("String 3");
+        node1.SetNext(node2);
+        node2.SetNext(node3);
+        Assert.Throws<ArgumentNullException>(() => node1.Exists(null!));
+    }
 
-        }
+    [Fact]
+    public void Append_TestIfAddsLast()
+    {
+        Node<String> node1 = new("String 1");
+        Node<String> node2 = new("String 2");
+        Node<String> node3 = new("String 3");
+        node1.SetNext(node2);
+        node2.SetNext(node3);
+        string data = "String4";
+        node1.Append(data);
+
+        Assert.Equal(node3.Next.Value, data);
+    }
+    [Fact]
+    public void Append_TestNullValue()
+    {
+        Node<String> node1 = new("String");
+        Node<String> node2 = new("String 2");
+        Node<String> node3 = new("String 3");
+        node1.SetNext(node2);
+        node2.SetNext(node3);
+        Assert.Throws<ArgumentNullException>(() => node1.Append(null!));
+
     }
 }
+
