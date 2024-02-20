694a2ff4f31a7e4f7d0a4df1f91dee67fdbc74fb
madams41 <madams41@ewu.edu>
2024-02-20 11:10:37 -0800

    created calculator

---
+++ Calculate/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate
+{
+    public class Calculator
+    {
+        //private IReadOnlyDictionary<TKey, TValue> MathematicalOperations;
+
+        public static int Add(int one, int two)
+        {
+            return one + two;
+        }
+
+        public static int Subtract(int one, int two)
+        {
+            return one - two;
+        }
+        public static int Multiple(int one, int two)
+        {
+            return one * two;
+        }
+
+        public static int Divide(int one, int two)
+        {
+            return one / two;
+        }
+        
+
+    }
+}
