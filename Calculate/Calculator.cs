8cca4d97c87a40df09ff054f1f3b6cb2b0430c1a
rkdeboer <145617897+rkdeboer@users.noreply.github.com>
2024-02-20 12:56:21 -0800

    Calculater namesoace

--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calculate
+namespace Calculate;
+public class Calculator
 {
-    public class Calculator
-    {
-        public static IReadOnlyDictionary<char, Func<int, int, int>> MathematicalOperations { get; } = new Dictionary<char, Func<int, int, int>>
+    public static IReadOnlyDictionary<char, Func<int, int, int>> MathematicalOperations { get; } = new Dictionary<char, Func<int, int, int>>
         {
              { '+', Add},
              { '-' , Subtract },
@@ -16,50 +15,50 @@ namespace Calculate
                {'*', Multiple }
         };
 
-        public static int Add(int one, int two)
-        {
-            return one + two;
-        }
+    public static int Add(int one, int two)
+    {
+        return one + two;
+    }
 
-        public static int Subtract(int one, int two)
-        {
-            return one - two;
-        }
-        public static int Multiple(int one, int two)
-        {
-            return one * two;
-        }
+    public static int Subtract(int one, int two)
+    {
+        return one - two;
+    }
+    public static int Multiple(int one, int two)
+    {
+        return one * two;
+    }
 
-        public static int Divide(int one, int two)
-        {
-            return one / two;
-        }
+    public static int Divide(int one, int two)
+    {
+        return one / two;
+    }
 
-        public static bool TryCalculate(string expression, out int result)
+    public static bool TryCalculate(string expression, out int result)
+    {
+        result = 0;
+        string[] input = expression.Split(" ");
+        if (input.Length != 3)
         {
-            result = 0;
-            string[] input = expression.Split(" ");
-            if (input.Length != 3)
+            return false;
+
+        }
+        if (int.TryParse(input[0], out int numOne) && int.TryParse(input[2], out int numtwo))
+        {
+            if (MathematicalOperations.TryGetValue(input[1][0], out Func<int, int, int>? operand))
             {
-                return false;
-
+                result = operand(numOne, numtwo);
+                return true;
             }
-            if (int.TryParse(input[0], out int numOne) && int.TryParse(input[2], out int numtwo))
-            {
-                if (MathematicalOperations.TryGetValue(input[1][0], out Func<int, int, int>? operand))
-                {
-                    result = operand(numOne, numtwo);
-                    return true;
-                }
-                return false;
-            }
-            else
-            {
-                return false;
-            }
-
-
+            return false;
         }
+        else
+        {
+            return false;
+        }
+
 
     }
+
 }
+
