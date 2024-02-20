bcd7808b11326826abf521f76b6ab3ef0b6a039c
rkdeboer <145617897+rkdeboer@users.noreply.github.com>
2024-02-20 11:12:50 -0800

    Program and Program tests

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -1,16 +1,18 @@
-﻿
+﻿namespace Calculate;
 public class Program
 {
     public delegate void WriteLineDelegate(string text);
     public delegate string ReadLineDelegate();
 
+
     public WriteLineDelegate WriteLine { get; init; }
     public ReadLineDelegate ReadLine { get; init; }
 
+
     public Program()
     {
-
         WriteLine ??= Console.WriteLine;
         ReadLine ??= Console.ReadLine;
     }
+
 }
