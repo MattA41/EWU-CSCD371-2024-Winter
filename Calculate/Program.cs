37dd063dcba4781504921889fcc018bbeffffe92
rkdeboer <145617897+rkdeboer@users.noreply.github.com>
2024-02-20 11:20:14 -0800

    Fixed warning for read

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -2,7 +2,7 @@
 public class Program
 {
     public delegate void WriteLineDelegate(string text);
-    public delegate string ReadLineDelegate();
+    public delegate string? ReadLineDelegate();
 
 
     public WriteLineDelegate WriteLine { get; init; }
