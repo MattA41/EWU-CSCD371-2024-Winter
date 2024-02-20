8d6dc8634928934230e6f7ad3c72ba7f9017b814
rkdeboer <145617897+rkdeboer@users.noreply.github.com>
2024-02-20 10:52:21 -0800

    Tests

---
+++ CalculateTests/ProgramTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace CalculateTests;
+public class ProgramTests
+{
+    [Fact]
+    public void TestConsoleOutput()
+    {
+        
+    }
+}
