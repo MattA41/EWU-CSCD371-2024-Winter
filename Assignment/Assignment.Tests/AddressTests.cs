c1d723526242b2ee843cc4335faa2e3f413b73de
madams41 <madams41@ewu.edu>
2024-02-26 12:33:08 -0800

    fixed test files

--- Assignment/Assignment.Tests/AddressTests.cs
+++ Assignment/Assignment.Tests/AddressTests.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Assignment;
+using Xunit;
 namespace Assignment.Tests;
 
     public class AddressTests
