aaf986a0fc77828a18cd57c4fe1305d42b58adf9
Anna-Mclenko <amikhailenko@ewu.edu>
2024-03-05 16:28:33 -0800

    fix some build issues

---
+++ Assignment/Person.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Assignment;
+
+public class Person(string firstName, string lastName, IAddress address, string emailAddress)
+    : IPerson
+{
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
+    public IAddress Address { get;set; } = address;
+    public string EmailAddress { get; set; } = emailAddress;
+}
